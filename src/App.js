8408a2ce9764ea0e155a965828f6be3c64f2ac8e
agnius <agnius@agnius.lt>
2019-04-27 18:20:48 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+// import { Provider } from 'react-redux';
+import { Route } from 'react-router';
+import Home from './components/Home';
+import Counter from './components/Counter';
+import FetchData from './components/FetchData';
+
+// import store from './store';
+// import saga from './sagas/';
+// import Main from './layouts/main';
+// import { sagaMiddleware } from './middleware';
+
+
+export default () => (
+  <div>
+    <Route exact path='/' component={Home} />
+    <Route path='/counter' component={Counter} />
+    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
+  </div>
+);
+
+
+// sagaMiddleware.run(saga);
